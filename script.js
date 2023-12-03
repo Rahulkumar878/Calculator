80f8f7f40d29bd74ef75a20e29d2cacc034868f4
Rahulkumar878 <78690062+Rahulkumar878@users.noreply.github.com>
2023-12-03 07:18:52 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function appendValue(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
